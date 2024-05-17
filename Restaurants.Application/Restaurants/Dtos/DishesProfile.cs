a48241dd7d3ffc9ba4ffe81e2ef746a4fcd83e14
KamilBeczek <kamilbeczek1998@gmail.com>
2024-05-17 22:15:57 +0200

    Hopefuly last commit fixing files in repository

---
+++ Restaurants.Application/Restaurants/Dtos/DishesProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Restaurants.Application.Dishes.Dtos;
+using Restaurants.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restaurants.Dtos
+{
+    public class DishesProfile : Profile
+    {
+        public DishesProfile() 
+        {
+            CreateMap<Dish, DishDto>();
+        }
+        
+    }
+}
---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaruantsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
+{
+    public class GetAllRestaruantsQueryHandler(ILogger<GetAllRestaruantsQueryHandler> logger,
+        IMapper mapper,
+        IRestaurantsRepository restaurantsRepository) : IRequestHandler<GetAllRestaruantsQuery, IEnumerable<RestaurantDto>>
+    {
+
+        public async Task<IEnumerable<RestaurantDto>> Handle(GetAllRestaruantsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Getting all restaurants");
+            var restaurants = await restaurantsRepository.GetAllAsync();
+            var restaurantsDto = mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
+
+            return restaurantsDto;
+        }
+
+    }
+}
